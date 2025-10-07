906b193bf8509bb65cb04b44b911a921079ed0d3
Will Bumbaugh <weeraphot@bumbaugh.com>
2025-10-06 17:33:23 -0700

    Initial Commit

---
+++ AcceloCube/AcceloCubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AcceloCubeApp.swift
+//  AcceloCube
+//
+//  Created by Weeraphot Bumbaugh on 10/6/25.
+//
+
+import SwiftUI
+
+@main
+struct AcceloCubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
